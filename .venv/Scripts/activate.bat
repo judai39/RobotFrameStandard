bae320a0e35dc06ec1181646b36eae61465d54ad
judai39 <haoyudu27@gmail.com>
2026-07-31 11:24:40 +0800

    venv环境配置信息变化

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\dkf11651\Desktop\RobotFrameStandard\.venv
+set VIRTUAL_ENV=E:\Project\RobotFrameworkStandard\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
